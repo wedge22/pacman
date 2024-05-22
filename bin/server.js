8f351805a62c6d9587d5bc7204aa965376bd87fb
wedge22 <ksh78@pm.me>
2024-05-22 16:03:52 -0700

    manual instrumentation

--- bin/server.js
+++ bin/server.js
@@ -5,6 +5,56 @@
  */
 require('@splunk/otel/instrument');
 
+// Import OpenTelemetry packages
+const { NodeTracerProvider } = require('@opentelemetry/sdk-node');
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-otlp-grpc');
+const { trace } = require('@opentelemetry/api');
+
+// Initialize the OpenTelemetry provider
+const provider = new NodeTracerProvider();
+
+// Create a tracer
+const tracer = trace.getTracer('pacman-game');
+
+// Express setup
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Middleware to start a span for each request
+app.use((req, res, next) => {
+  const span = tracer.startSpan('http_request', {
+    attributes: {
+      'http.method': req.method,
+      'http.url': req.url,
+    },
+  });
+  res.on('finish', () => {
+    span.setAttribute('http.status_code', res.statusCode);
+    span.end();
+  });
+  next();
+});
+
+// Example of a manually instrumented route
+app.get('/', (req, res) => {
+  // Start a span for the '/' route
+  const span = tracer.startSpan('GET /');
+  try {
+    res.send('Pacman game server');
+    span.setStatus({ code: 0 }); // OK
+  } catch (err) {
+    span.setStatus({ code: 2, message: err.message }); // Error
+  } finally {
+    span.end();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Pacman game app listening at http://localhost:${port}`);
+});
+
 var app = require('../app');
 //var debug = require('debug')('pacman:server');
 var http = require('http');
