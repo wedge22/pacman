13c2c47ec49f0517b426a49b38ab57d6c56f3a99
wedge22 <ksh78@pm.me>
2024-07-24 13:45:58 -0700

    updating with CDN

--- splunk-instrumentation.js
+++ splunk-instrumentation.js
@@ -1,7 +1,8 @@
-import SplunkOtelWeb from '@splunk/otel-web';
-SplunkOtelWeb.init({
-   realm: "us1",
-   rumAccessToken: "bRa9gAn966GFzFeIIgw0rg",
-   applicationName: "ksh-pacman",
-   deploymentEnvironment: "lab"
+import SplunkOtelWeb from 'https://unpkg.com/@splunk/otel-web@0.19.0/dist/index.js';
+
+SplunkOtelWeb.init({ 
+    realm: "us1", 
+    rumAccessToken: "bRa9gAn966GFzFeIIgw0rg", 
+    applicationName: "ksh-pacman", 
+    deploymentEnvironment: "lab" 
 });
