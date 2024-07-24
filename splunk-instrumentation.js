b54132c989d650519959688a83cfa6417926077f
wedge22 <ksh78@pm.me>
2024-07-24 14:08:53 -0700

    updates

---
+++ splunk-instrumentation.js
@@ -0,0 +1,7 @@
+import SplunkOtelWeb from '@splunk/otel-web';
+SplunkOtelWeb.init({
+   realm: "us1",
+   rumAccessToken: "bRa9gAn966GFzFeIIgw0rg",
+   applicationName: "app_name",
+   deploymentEnvironment: "lab"
+});
